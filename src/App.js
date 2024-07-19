b29a47aebf66ab2e405855e94e29173b23cd1bf9
Daniel <Daniel_Makalski13@hotmail.ca>
2024-07-18 22:37:48 -0400

    done

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Pokedex from './Pokedex';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Pokedex</h1>
+      <Pokedex />
+    </div>
+  );
+}
+
+export default App;
